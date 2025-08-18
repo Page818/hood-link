85419b24941f9f7b23eb1eebacc301ade9ed4b54
Page818 <pguo2000@gmail.com>
2025-08-18 16:31:26 +0800

    20250818-安裝@mdi/font

--- front/src/main.js
+++ front/src/main.js
@@ -8,6 +8,8 @@ import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 const vuetify = createVuetify({ components, directives })
 const app = createApp(App)
